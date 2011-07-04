c1eb3468f943abaad081e4218674dd88c792ddc5
Felix Sun <sunfmin@gmail.com>
2011-07-04 23:23:04 +0800

    different func call

---
+++ different_func_invocation_of_struct.go
@@ -0,0 +1,24 @@
+package main
+import(
+	"fmt"
+)
+
+type Man struct {
+	Name string
+}
+
+
+func (h Man) Follow(who ...string) {
+	fmt.Printf("%s just followed: \n", h.Name)
+	for _, v := range(who) {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	h := &Man{"Chris"}
+
+	h.Follow("Aoi", "Box")
+
+	Man.Follow(*h, "Sora", "Box")
+}
