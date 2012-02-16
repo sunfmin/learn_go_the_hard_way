67752e4af262cc15efee067af7f769adc775d19e
Felix Sun <sunfmin@gmail.com>
2012-02-16 20:29:37 +0800

    add internal compile error

---
+++ interface_array_to_other_array.go
@@ -0,0 +1,31 @@
+package main
+import(
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	mapArray := []interface{}{
+		map[string]interface{}{"key1": 1},
+		map[string]interface{}{"key2": "2"},
+	}
+
+	fmt.Println(mapArray)
+
+	fmt.Println(reflect.TypeOf(mapArray))
+
+	var newMapArray []map[string]interface{}
+/*	newMapArray = mapArray*/
+/*	newMapArray = []map[string]interface{}(mapArray)*/
+	for _, v := range(mapArray) {
+		mapValue, ok := v.(map[string]interface{})
+		if ok {
+			newMapArray = append(newMapArray, mapValue)
+		}
+	}
+
+	fmt.Println(newMapArray)
+	fmt.Println(reflect.TypeOf(newMapArray))
+
+}
+
---
+++ internal_compile_error.go
@@ -0,0 +1,8 @@
+package top
+
+type taobaoMap map[string]interface{}
+
+func (m taobaoMap) unwrap() (r interface{}, count string) {
+        convertedMap := map[string]interface{}(m)
+        return taobaoMap(convertedMap).unwrap(), count
+}
