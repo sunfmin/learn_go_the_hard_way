dca03280ae8115d6c34107286ee2e2f0998ba761
Felix Sun <sunfmin@gmail.com>
2012-02-21 00:38:33 +0800

    what is quick.Check

---
+++ testing_quick.go
@@ -0,0 +1,38 @@
+package main
+
+import(
+	"testing/quick"
+	"fmt"
+)
+
+/*func TestOddMultipleOfThree(t *testing.T) {
+	f := func(x int) bool {
+		y := OddMultipleOfThree(x)
+		return y%2 == 1 && y%3 == 0
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}*/
+
+type Person struct {
+	Name string
+	Year int
+/*	Parent *Person*/
+}
+
+func main() {
+	intf := func(x int) bool {
+		fmt.Println(x)
+		return true
+	}
+
+	quick.Check(intf, nil)
+
+	personf := func(p *Person) bool {
+		fmt.Printf("%+v\n", p)
+		return true
+	}
+	quick.Check(personf, nil)
+}
+
