f3ea81452f52605f2d296d0afbbbb18fd3b893ba
Felix Sun <sunfmin@gmail.com>
2012-02-26 19:22:34 +0800

    call func by string

---
+++ call_func_by_string.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+	"fmt"
+	"reflect"
+)
+
+type User struct {
+	Name string
+}
+
+
+
+func (u *User) Hello() {
+	fmt.Println("Hello", u.Name)
+}
+
+func main() {
+	u := &User{"Felix"}
+	v := reflect.ValueOf(u)
+	fmt.Println(v)
+	fmt.Println(v.MethodByName("Hello").Call([]reflect.Value{}))
+}
