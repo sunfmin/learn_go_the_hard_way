eb64f6cac312d0bc3d82baa647d41829c94d8ae0
Felix Sun <sunfmin@gmail.com>
2012-03-07 16:37:55 +0800

    add out of memory

--- out_of_memory.go
+++ out_of_memory.go
@@ -1,21 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-	/*	"unsafe"*/
-)
-
 func main() {
-	m := "Lv"
-
-	/*	var mAddress *string*/
-
-	m1 := &m
-	mAddress := &m1
 	for {
-		mAddress1 := &mAddress
-		fmt.Println("address's address type: ", reflect.TypeOf(mAddress1))
-	}
+		_ = make([]byte, 600<<20)
 
+	}
 }
