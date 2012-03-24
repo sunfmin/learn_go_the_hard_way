9695e15e5a63a77197e57a76f80564e56cc069ca
Felix Sun <sunfmin@gmail.com>
2012-03-24 13:28:55 +0800

    channel

--- doc.go
+++ doc.go
@@ -1,6 +1,14 @@
 /*
+	This package is intend for you to learning The Go Programming Language.
+
+	All the examples are organized into packages, that each package try to explain the small details
+	for a specific area in Go, and some standard package usage. Or the Language Specification.
+
+	Normally you can go into each package directory, and run:
+
+		go test
+
 
 
 */
-
 package learn_go_the_hard_way
--- syncing_many_goroutines_with_channel/channel_test.go
+++ syncing_many_goroutines_with_channel/channel_test.go
@@ -1,18 +1,18 @@
 // http://rouninurashima.wordpress.com/2012/01/18/17/
-package main
+package syncing_many_goroutines_with_channel
 
 import (
 	"fmt"
 )
 
-func main() {
+func ExampleRun() {
 	c := make(chan int)
 	for i := 0; i < 1000; i++ {
 		go func() {
 			for {
 				select {
 				case c <- 1:
-				case c <- 0:
+				case c <- 2:
 				}
 			}
 		}()
